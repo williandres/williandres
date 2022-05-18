b26a0da1cbec7154e9a4926cbf8eecfa31745da9
Willian <sca.willian@gmail.com>
2022-05-18 03:29:51 -0500

    Chango and me, template

---
+++ me/js/main.js
@@ -0,0 +1,47 @@
+jQuery(window).on('load', function() {
+	"use strict";
+    // HIDE PRELOADER
+    $(".preloader").addClass("hide-preloader"); 
+
+    // SHOW/ANIMATE ANIMATION CONTAINER
+    setTimeout(function(){
+
+        $("#intro .animation-container").each(function() {
+
+            var e = $(this);
+
+            setTimeout(function(){
+
+                e.addClass("run-animation");
+
+            }, e.data("animation-delay") );
+
+        });
+
+    }, 700 );
+
+});
+
+jQuery(document).ready(function($) {
+	"use strict";
+    // SMOOTH SCROLL FOR SAME PAGE LINKS
+    $(document).on('click', 'a.smooth-scroll', function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top - 80
+        }, 500);
+    });
+    // SCROLL REVEAL SETUP
+    window.sr = ScrollReveal();
+    sr.reveal(".scroll-animated-from-right", { 
+        duration: 600,
+        delay: 0,
+        origin: "right",
+        rotate: { x: 0, y: 0, z: 0 },
+        opacity: 0,
+        distance: "20vh",
+        viewFactor: 0.4,
+        scale: 1,
+    });
+
+});
